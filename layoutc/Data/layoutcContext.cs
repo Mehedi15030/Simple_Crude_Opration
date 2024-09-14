acb2afd7c72910d864867957e4ab8a3493e1ec93
Mehedi15030 <mehedicse15030@gmail.com>
2024-09-14 21:35:06 +0600

    Add project files.

---
+++ layoutc/Data/layoutcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using layoutc.Models;
+
+namespace layoutc.Data
+{
+    public class layoutcContext : DbContext
+    {
+        public layoutcContext (DbContextOptions<layoutcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<layoutc.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ layoutc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace layoutc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
